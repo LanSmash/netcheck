3dd862f2d5453a73263d85dbc71ed16b066781c9
webberist <me@webberist.com>
2015-06-10 15:04:22 +1000

    added DNS, RTMP and normal HTTP checks

--- main.rb
+++ main.rb
@@ -1,15 +1,32 @@
-require 'net/http'
-require 'openssl'
 
 #counters for statistics at end
 success = 0
 failed = 0
 tests = 0
 
+require 'net/http'
+begin
+  print "\nAttempting HTTP connection to www.google.com port 80...\n"
+  tests += 1
+  http = Net::HTTP.new('www.google.com', '80')
+  http.open_timeout=3
+  http.get('/')
+rescue => e
+  puts e
+  failed += 1
+  print "HTTP: Failed - something failed\n"
+else
+  print "HTTP: Success - connection successful\n"
+  success += 1
+end
+
+require 'net/http'
+require 'openssl'
+
 # Grab the cert received out of band
-# downloaded using the command: openssl s_client -showcerts -connect originenergy.com.au:443 </dev/null 2>/dev/null|openssl x509 -outform PEM >originenergy.com.au.cer
-cert_code = File.read 'originenergy.com.au.cer'
-#cert_code = File.read 'www.clickenergy.com.au.cer'
+# downloaded using the command: openssl s_client -showcerts -connect www.originenergy.com.au:443 </dev/null 2>/dev/null|openssl x509 -outform PEM >www.originenergy.com.au.cer
+#cert_code = File.read 'www.originenergy.com.au.cer'
+cert_code = File.read 'www.clickenergy.com.au.cer'
 downloaded_cert = OpenSSL::X509::Certificate.new(cert_code)
 
 # Tells us whether the private keys on the passed certificates match
@@ -26,8 +43,8 @@ def same_public_key?(ref_cert, actual_cert)
 end
 
 # Configure a new HTTP object
-http = Net::HTTP.new('originenergy.com.au', 443)
-#http = Net::HTTP.new('www.clickenergy.com.au', 443)
+#http = Net::HTTP.new('www.originenergy.com.au', 443)
+http = Net::HTTP.new('www.clickenergy.com.au', 443)
 http.use_ssl = true
 
 # We will verify against our CAs in the root store, and with VERIFY_NONE
@@ -72,9 +89,9 @@ end
 
 # This request will fail if the cert doesn't match
 begin 
-  print "Attempting HTTPS connection using pinned certificate...\n"
-  res = http.get '/'
   tests += 1
+  print "\nAttempting HTTPS connection using pinned certificate...\n"
+  res = http.get '/'
 rescue => e
   print "HTTPS: Failed -\n"
   puts e
@@ -87,13 +104,13 @@ end
 
 require 'net/http'
 begin
-  print "Attempting HTTP connection to nonexistent server...\n"
+  print "\nAttempting HTTP connection to nonexistent server port 80...\n"
   tests += 1
-  http = Net::HTTP.new('198.51.100.57')
+  http = Net::HTTP.new('198.51.100.57', '80')
   http.open_timeout=3
   http.get('/index.html')
 rescue Net::OpenTimeout
-  print "HTTP Proxy: Success - transparent proxy not detected\n"
+  print "HTTP Proxy: Success - transparent proxy not detected (timeout)\n"
   success += 1
 rescue => e
   puts e
@@ -119,6 +136,53 @@ end
 #  success += 1
 #end
 
+require 'net/http'
+begin
+  print "\nAttempting connection to nonexistent server on port 53...\n"
+  tests += 1
+  http = Net::HTTP.new('198.51.100.57', '53')
+  http.open_timeout=3
+  http.get('/index.html')
+rescue Net::OpenTimeout
+  print "DNS: Success - transparent dns intercepts not detected (timeout)\n"
+  success += 1
+rescue => e
+  puts e
+  failed += 1
+  print "DNS: Failed - something snagged our connection attempt\n"
+end
+
+require 'net/http'
+begin
+  print "\nAttempting connection to RTMP Live-syd.Twitch.TV server on port 1935...\n"
+  tests += 1
+  http = Net::HTTP.new('Live-syd.Twitch.TV', '1935')
+  http.open_timeout=3
+  http.get('/index.html')
+rescue => e
+  puts e
+  failed += 1
+  print "RTMP: Failed - something failed\n"
+else
+  print "RTMP: Success - port opened\n"
+  success += 1
+end
+
+require 'net/http'
+begin
+  print "\nAttempting connection to RTMP Live.Twitch.TV server on port 1935...\n"
+  tests += 1
+  http = Net::HTTP.new('Live.Twitch.TV', '1935')
+  http.open_timeout=3
+  http.get('/index.html')
+rescue => e
+  puts e
+  failed += 1
+  print "RTMP: Failed - something failed\n"
+else
+  print "RTMP: Success - port opened\n"
+  success += 1
+end
 
 puts "Successes: #{success}/#{tests}."
 puts "Fails: #{failed}/#{tests}."
